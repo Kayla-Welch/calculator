25c2c3b20a8f7cfa1fd90ab085869f1789d701d3
Kayla-Welch <122100249+Kayla-Welch@users.noreply.github.com>
2023-01-12 13:18:50 -0500

    added the main() function and checked if the main file was the one ran at first startup

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 from functions import *
 
 running = True
+def main():
+    while running:
+        clear()
+        print("calculator developed by kayla welch")
+        print("1; addition")
+        print("2: subtraction")
+        print("3: multiplication")
+        print("4: division")
+        print("other: exit")
 
-while running:
-    clear()
-    print("calculator developed by kayla welch")
-    print("1; addition")
-    print("2: subtraction")
-    print("3: multiplication")
-    print("4: division")
-    print("other: exit")
-
-    operationSelector = input("input number: ")
-    clear()
+        operationSelector = input("input number: ")
+        clear()
 
     if operationSelector == "1":
         print(f"sum: {addition()}")
@@ -26,3 +26,6 @@ while running:
         print(f"quotient: {division()}")
     else:
         running = False
+
+if __name__ == "__main__":
+    main()
