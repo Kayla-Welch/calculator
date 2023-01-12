638407167733e74ff0f4ad7091dd729f47654f5b
Kayla-Welch <122100249+Kayla-Welch@users.noreply.github.com>
2023-01-12 13:14:26 -0500

    compacted parts of the math operations functions into a smaller function, also began labelling.

--- functions.py
+++ functions.py
@@ -2,57 +2,54 @@ def clear():
     import os
 
     os.system("clear")
+
+def numberInput():
+    
+
+    try:
+        # allows the numbers that will be formed to be used in other functions
+        global number1
+        global number2
+
+        # asks the user for the numbers that will be used for operations
+        number1 = int(input("number 1: "))
+        number2 = int(input("number 1: "))
+    
+    # checks if the number inputted is actually a number and not some other character
+    except ValueError:
+        print("input a number instead of a letter or symbol")
+
 class MathOperations:
     def addition():
-        try:
-            number1 = int(input("number 1: "))
-            number2 = int(input("number 1: "))
-        except ValueError:
-            print("input a number instead of a letter or symbol")
+        numberInput()
 
         additionSum = number1 + number2
 
         return additionSum
 
     def subtraction():
-        try:
-            number1 = int(input("number 1: "))
-            number2 = int(input("number 1: "))
-        except ValueError:
-            print("input a number instead of a letter or symbol")
+        numberInput()
 
         subtractionDifference = number1 - number2
 
         return subtractionDifference
 
     def multiplication():
-        try:
-            number1 = int(input("number 1: "))
-            number2 = int(input("number 1: "))
-        except ValueError:
-            print("input a number instead of a letter or symbol")
+        numberInput()
 
         multiplicationProduct = number1 * number2
 
         return multiplicationProduct
 
     def division():
-        try:
-            number1 = int(input("number 1: "))
-            number2 = int(input("number 1: "))
-        except ValueError:
-            print("input a number instead of a letter or symbol")
+        numberInput()
 
         divisionQuotient = number1 / number2
 
         return divisionQuotient
 
     def exponent():
-        try:
-            number1 = int(input("number 1: "))
-            number2 = int(input("number 1: "))
-        except ValueError:
-            print("input a number instead of a letter or symbol")
+        numberInput()
 
         exponentApplication = number1^number2
 
